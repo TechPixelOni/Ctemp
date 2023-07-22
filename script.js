61798ff7b6357a4d6e66d4c30658a32381e664f1
TechPixelOni <dejonmuhammad@gmail.com>
2023-07-22 13:55:53 -0500

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,60 +1,33 @@
 // Function to fetch data from a JSON file
 function fetchData(url) {
-  // Check if the URL is using the file:// protocol
-  if (url.startsWith('file://')) {
-    // Use XMLHttpRequest to fetch data when using the file:// protocol
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-          if (xhr.status === 200) {
-            try {
-              const data = JSON.parse(xhr.responseText);
-              resolve(data);
-            } catch (error) {
-              reject(error);
-            }
-          } else {
-            reject(new Error(`Failed to fetch data: ${xhr.status} ${xhr.statusText}`));
-          }
-        }
-      };
-      xhr.open('GET', url);
-      xhr.send();
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error('Failed to fetch data:', error);
+      throw error; // Re-throw the error to propagate it
     });
-  } else {
-    // Use fetch for other protocols
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .catch(error => {
-        console.error('Failed to fetch data:', error);
-        throw error; // Re-throw the error to propagate it
-      });
+}
+
+// Function to clear all child nodes of an element
+function clearChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
   }
 }
 
-// Function to populate the groups in the HTML
-function populateGroups() {
-  const groupList = document.getElementById('group-list');
-  fetchData('/data/groups.json')
-    .then(groups => {
-      groups.forEach(group => {
-        const listItem = document.createElement('li');
-        const link = document.createElement('a');
-        link.href = `#${group.id}`;
-        link.textContent = group.name;
-        listItem.appendChild(link);
-        groupList.appendChild(listItem);
-      });
-    })
-    .catch(error => {
-      console.error('Failed to fetch groups:', error);
-    });
+// Function to create an element with text content
+function createElement(tag, text, className) {
+  const element = document.createElement(tag);
+  element.textContent = text;
+  if (className) {
+    element.className = className;
+  }
+  return element;
 }
 
 // Function to copy the template content to the clipboard
@@ -68,6 +41,32 @@ function copyTemplate(content) {
       // Handle the error or display an error message to the user
     });
 }
+
+// Function to fetch groups and populate group list on page load
+document.addEventListener('DOMContentLoaded', () => {
+  const groupList = document.getElementById('group-list');
+  fetchData('/data/groups.json')
+    .then(groups => {
+      groups.forEach(group => {
+        const listItem = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = `templates.html#${group.id}`;
+        link.textContent = group.name;
+        listItem.appendChild(link);
+        groupList.appendChild(listItem);
+      });
+    })
+    .catch(error => {
+      console.error('Failed to fetch groups:', error);
+    });
+
+  const groupId = window.location.hash.slice(1);
+  if (groupId) {
+    fetchTemplatesByGroup(groupId);
+  } else {
+    fetchAllTemplates();
+  }
+});
 
 // Function to fetch templates based on group ID and populate template list
 function fetchTemplatesByGroup(groupId) {
@@ -86,51 +85,6 @@ function fetchTemplatesByGroup(groupId) {
     });
 }
 
-// Function to create template list items
-function createTemplateListItem(template) {
-  const listItem = document.createElement('li');
-  const titleElement = document.createElement('h3');
-  titleElement.textContent = template.title;
-  listItem.appendChild(titleElement);
-  const copyButton = document.createElement('button');
-  copyButton.textContent = 'Copy';
-  copyButton.classList.add('copy-btn');
-  copyButton.addEventListener('click', () => copyTemplate(template.content));
-  listItem.appendChild(copyButton);
-  return listItem;
-}
-
-// Function to clear all child nodes of an element
-function clearChildren(element) {
-  while (element.firstChild) {
-    element.removeChild(element.firstChild);
-  }
-}
-
-// Function to handle hash changes and fetch templates accordingly
-function handleHashChange() {
-  const groupId = window.location.hash.slice(1);
-  if (groupId) {
-    fetchTemplatesByGroup(groupId);
-  } else {
-    fetchAllTemplates();
-  }
-}
-
-// Function to handle scroll and show/hide the footer
-function handleScroll() {
-  const footer = document.querySelector('.footer');
-  const isScrolledToBottom = () => {
-    return window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
-  };
-
-  if (isScrolledToBottom()) {
-    footer.classList.add('visible');
-  } else {
-    footer.classList.remove('visible');
-  }
-}
-
 // Function to fetch all templates and populate template list
 function fetchAllTemplates() {
   const templateList = document.getElementById('template-list');
@@ -147,11 +101,43 @@ function fetchAllTemplates() {
     });
 }
 
-// Call the functions when the page is loaded and when the hash changes
-document.addEventListener('DOMContentLoaded', () => {
-  populateGroups();
-  handleHashChange();
+// Function to create template list items
+function createTemplateListItem(template) {
+  const listItem = document.createElement('li');
+  const titleElement = createElement('h3', template.title);
+  listItem.appendChild(titleElement);
+  const copyButton = createElement('button', 'Copy', 'copy-btn');
+  copyButton.addEventListener('click', () => copyTemplate(template.content));
+  listItem.appendChild(copyButton);
+  return listItem;
+}
+
+// Handle hash changes and fetch templates accordingly
+window.addEventListener('hashchange', () => {
+  const groupId = window.location.hash.slice(1);
+  if (groupId) {
+    fetchTemplatesByGroup(groupId);
+  } else {
+    fetchAllTemplates();
+  }
 });
 
-window.addEventListener('hashchange', handleHashChange);
-window.addEventListener('scroll', handleScroll);
+// Footer
+window.addEventListener("DOMContentLoaded", function() {
+  const footer = document.querySelector(".footer");
+  const isScrolledToBottom = () => {
+    return window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+  };
+
+  const handleScroll = () => {
+    if (isScrolledToBottom()) {
+      footer.classList.add("visible");
+    } else {
+      footer.classList.remove("visible");
+    }
+  };
+
+  handleScroll();
+  window.addEventListener("scroll", handleScroll);
+});
+
